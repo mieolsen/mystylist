b909109491163fdc168091d805e460ddd5c87939
Mie Olsen <mielangsted@gmail.com>
2020-06-01 21:59:04 +0200

    tilføjer sider
    .

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function styleFunction() { document.getElementById("styleDropdown").classList.to
 
 function behandlingFunction() { document.getElementById("behandlingDropdown").classList.toggle("visboks");
 }
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropknap1'  ) && !event.target.matches('.dropknap2' )) {
+    var dropdowns = document.getElementsByClassName("indhold");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('visboks')) {
+        openDropdown.classList.remove('visboks');
+      }
+    }
+  }
+}
