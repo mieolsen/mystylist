77b9d60103e12f421afd10a8b82099a8ec1e0131
Mie Olsen <mielangsted@gmail.com>
2020-06-07 10:15:23 +0200

    fejlrettelser efter test

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-// JavaScript Document
 
-function styleFunction() { document.getElementById("styleDropdown").classList.toggle("visboks");
-}
-
-function behandlingFunction() { document.getElementById("behandlingDropdown").classList.toggle("visboks");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropknap1'  ) && !event.target.matches('.dropknap2' )) {
-    var dropdowns = document.getElementsByClassName("indhold");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('visboks')) {
-        openDropdown.classList.remove('visboks');
-      }
-    }
+function myFunction() {
+  var x = document.getElementById("navigation");
+  if (x.className === "nav") {
+    x.className += " mobile";
+  } else {
+    x.className = "nav";
   }
 }
+
+
+
+
+
+
 
 
 var slideIndex = 1;
